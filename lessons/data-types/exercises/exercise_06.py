24bd55f8c306cd30e34e82f1bb71c8e357905c03
Martin <arizagamartin3@gmail.com>
2023-03-08 20:33:01 +0100

    meh
    nada

--- lessons/data-types/exercises/exercise_06.py
+++ lessons/data-types/exercises/exercise_06.py
@@ -3,7 +3,7 @@ my_set = {-2,-1,0}
 my_string = "abc"
 my_list_2 = "I'm learning Python".split()
 my_dict = {
-    "seconds": (24*3600),
+    "seconds": (24*60*60),
     "minutes": (24*60),
     "hours": 24,
 }
